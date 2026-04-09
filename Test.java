9620147c82fcebf7a4ff1e1631d43cb9fd276b0e
Phumelele-1103 <skadonaven91@gmail.com>
2026-04-09 15:03:03 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,42 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test{
-  public static void main(String[] args){
-    System.out.print("Hello");
-  }
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    ArrayList<Login> users = new ArrayList<>();
+    boolean isRunning = true;
+
+    System.out.println("Hello, welcome to the ChatApp.");
+    while(isRunning){
+      System.out.println("Main Menu: ");
+      System.out.println("1. Register.");
+      System.out.println("2. Login.");
+      System.out.println("3. Exit.");
+      System.out.print("Choose an option: ");
+
+      int choice = sc.nextInt();
+      sc.nextLine();
+
+     switch(choice){
+      //prompt user to create account
+       case 1:
+         System.out.println("Register: ");
+         System.out.print("Create a username: ");
+         String username = sc.nextLine();
+
+         //validate username
+         boolean validUsername = username.contains("_") && username.length() <= 5;
+         
+
+         
+     }
+    }
+    
+
+    
+    }
 }
